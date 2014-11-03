9f07e109341ce40e47956c1e21dea7c51b978988
michaelaranda0 <michael.aranda@hp.com>
2014-11-03 11:24:17 -0800

    Added index() in PostsController.php

--- cakephp/app/Controller/PostsController.php
+++ cakephp/app/Controller/PostsController.php
@@ -2,4 +2,8 @@
 
 class PostsController extends AppController {
     public $helpers = array('Html', 'Form');
+
+    public function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
 }
