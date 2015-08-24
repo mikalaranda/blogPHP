f019e13fd9f3003724eb69c80dd78133ee1527a4
mikalaranda <michaelaranda0@gmail.com>
2015-08-23 21:23:31 -0700

    New functionality for adding posts and data validation

--- cakephp/app/Controller/PostsController.php
+++ cakephp/app/Controller/PostsController.php
@@ -1,7 +1,8 @@
 <?php
 
 class PostsController extends AppController {
-    public $helpers = array('Html', 'Form');
+    public $helpers = array('Html', 'Form', 'Session');
+    public $components = array('Session');
 
     public function index() {
         $this->set('posts', $this->Post->find('all'));
@@ -18,5 +19,16 @@ class PostsController extends AppController {
         }
         
         $this->set('post', $post);
+    }
+
+    public function add() {
+        if ($this->request->is('post')) {
+            $this->Post->create();
+            if ($this->Post->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
     }
 }
